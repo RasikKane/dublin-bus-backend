68faf433d129cb94206304fcba5a20d013134d67
sarvesh-gadgil <60133683+sarvesh-gadgil@users.noreply.github.com>
2020-07-31 18:48:31 +0530

    Modified the cron for weather

--- dublin_bus_backend/weather/weather_cron.py
+++ dublin_bus_backend/weather/weather_cron.py
@@ -1,10 +1,29 @@
 import requests
 from weather.models import Weather
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Open Weather API URL
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/forecast"
 WEATHER_PARAMS = {'appid': "0b877c10f2c8044a1668be4f89c01784", 'id': '2964574'}
+
+
+def save_update_weather_data(date_weather, hour_of_day, feels_like, wind_speed, weather_id, temperature,
+                             temperature_min, temperature_max, humidity, weather_main,
+                             weather_description):
+    obj, created = Weather.objects.update_or_create(
+        date_weather=date_weather, hour_of_day=hour_of_day,
+        defaults={
+            'feels_like': feels_like,
+            'wind_speed': wind_speed,
+            'weather_id': weather_id,
+            'temp': temperature,
+            'temp_min': temperature_min,
+            'temp_max': temperature_max,
+            'humidity': humidity,
+            'weather_main': weather_main,
+            'weather_description': weather_description
+        },
+    )
 
 
 def weather_cron_job():
@@ -14,39 +33,53 @@ def weather_cron_job():
 
         # Check for response code
         if response['cod'] == "200":
+            len_list = len(response['list'])
+
             # Iterating the predictions
-            for predictions in response['list']:
+            for count, predictions in enumerate(response['list'], start=1):
                 try:
                     # Convert the required values
                     date_weather_in_date = datetime.fromtimestamp(predictions['dt'])
                     date_weather = date_weather_in_date.strftime('%Y-%m-%d')
                     hour_of_day = date_weather_in_date.hour
-                    feels_like = str(round(predictions['main']['feels_like'] - 273.15))
-                    wind_speed = str(round(predictions['wind']['speed']))
+                    feels_like = "{:.1f}".format(predictions['main']['feels_like'] - 273.15)
+                    wind_speed = "{:.1f}".format(predictions['wind']['speed'])
                     weather_id = str(predictions['weather'][0]['id'])
-                    temperature = str(round(predictions['main']['temp'] - 273.15))
-                    temperature_min = str(round(predictions['main']['temp_min'] - 273.15))
-                    temperature_max = str(round(predictions['main']['temp_max'] - 273.15))
+                    temperature = "{:.1f}".format(predictions['main']['temp'] - 273.15)
+                    temperature_min = "{:.1f}".format(predictions['main']['temp_min'] - 273.15)
+                    temperature_max = "{:.1f}".format(predictions['main']['temp_max'] - 273.15)
                     humidity = predictions['main']['humidity']
                     weather_main = predictions['weather'][0]['main']
                     weather_description = predictions['weather'][0]['description']
 
-                    # Create/update in db
-                    obj, created = Weather.objects.update_or_create(
-                        date_weather=date_weather, hour_of_day=hour_of_day,
-                        defaults={
-                            'feels_like': feels_like,
-                            'wind_speed': wind_speed,
-                            'weather_id': weather_id,
-                            'temp': temperature,
-                            'temp_min': temperature_min,
-                            'temp_max': temperature_max,
-                            'humidity': humidity,
-                            'weather_main': weather_main,
-                            'weather_description': weather_description
-                        },
-                    )
+                    # Make default first entry
+                    save_update_weather_data(date_weather, hour_of_day, feels_like, wind_speed, weather_id, temperature,
+                                             temperature_min, temperature_max, humidity, weather_main,
+                                             weather_description)
+
+                    # for last entry in cron job; we need additional 3 hour data; same data for last hr is repeated
+                    if count == len_list:
+                        print("count == len_list")
+                        for i in range(3):
+                            hour_of_day = hour_of_day + 1
+                            # Save in db
+                            save_update_weather_data(date_weather, hour_of_day, feels_like, wind_speed, weather_id,
+                                                     temperature,
+                                                     temperature_min, temperature_max, humidity, weather_main,
+                                                     weather_description)
+
+                    # 1st 3 hr data for a day = Extended [23,24,25] hr data for last day
+                    elif hour_of_day in [0, 1, 2]:
+                        hour_of_day = hour_of_day + 24
+                        date_weather = (date_weather_in_date - timedelta(days=1)).strftime('%Y-%m-%d')
+                        # Save in db
+                        save_update_weather_data(date_weather, hour_of_day, feels_like, wind_speed, weather_id,
+                                                 temperature,
+                                                 temperature_min, temperature_max, humidity, weather_main,
+                                                 weather_description)
                 except Exception as e:
-                    print('exception in weather_cron_job for loop', e)
+                    # print('exception in weather_cron_job for loop', e)
+                    pass
     except Exception as e:
-        print('exception in weather_cron_job', e)
+        # print('exception in weather_cron_job', e)
+        pass
