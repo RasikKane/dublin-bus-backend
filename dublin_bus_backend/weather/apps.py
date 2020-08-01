74578631cc68a1953e902984526b913200950aa3
sarvesh-gadgil <60133683+sarvesh-gadgil@users.noreply.github.com>
2020-08-01 16:54:54 +0530

    Modified the cron for weather

--- dublin_bus_backend/weather/apps.py
+++ dublin_bus_backend/weather/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class WeatherConfig(AppConfig):
     name = 'weather'
 
-    def ready(self):
-        from weather import weather_cron_starter
-        weather_cron_starter.start()
+    # def ready(self):
+    #     from weather import weather_cron_starter
+    #     weather_cron_starter.start()
